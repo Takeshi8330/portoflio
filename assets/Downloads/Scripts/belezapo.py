1f7420ed50bd225d2cce647375f79be504cefa6c
Takeshi8330 <henriquejurcamm@gmail.com>
2023-08-14 23:31:47 -0300

    first commit

---
+++ assets/Downloads/Scripts/belezapo.py
@@ -0,0 +1,4 @@
+print('Tá safe po')
+input('Pressione enter para sair...')
+#adadadadadadadada
+#adadadadadadadada#adadadadadadadada#adadadadadadadada#adadadadadadadada#adadadadadadadada#adadadadadadadada#adadadadadadadada#adadad
